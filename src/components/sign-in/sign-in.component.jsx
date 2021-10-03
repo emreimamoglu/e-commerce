0a956a9c5c520eb7d59ba5142193bf195bba8e2b
emre imamoğlu <imamogluemree@gmail.com>
2021-10-03 16:58:45 +0300

    Added signin and signup with email and password

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import {auth , signInWithGoogle } from "../../firebase/firebase.utils";
 import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
@@ -14,9 +14,18 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async(event) => {
     event.preventDefault();
-
+    const {email , password} = this.state;
+    try{
+      auth.signInWithEmailAndPassword(email,password);
+      this.setState({
+        email : '',
+        password : ''
+      })
+    }catch(err){
+      console.error(err)
+    }
     this.setState({ email: "", password: "" });
   };
 
@@ -49,7 +58,7 @@ class SignIn extends React.Component {
             handleChange={this.handleChange}
           />
           <div className="buttons">
-            <CustomButton type="submit">Sign In</CustomButton>
+            <CustomButton type="submit" onClick = {this.handleSubmit}>Sign In</CustomButton>
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
               Sign In With Google
             </CustomButton>
--- src/pages/sisu/sisu.component.jsx
+++ src/pages/sisu/sisu.component.jsx
@@ -1,11 +1,13 @@
 import React from "react"; 
 import './sisu.styles.scss';
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 const Sisu = () => {
     return(
         <div className='sisu'>
             <SignIn/>
+            <SignUp/>
         </div>
     );
 }
