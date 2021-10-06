4c54f31573ed416d8dfda1be70cb5de4304bb1e5
Emre <emre.imamoglu@aartimuhendislik.com>
2021-10-06 22:21:57 +0300

    Added checkout adding and removing feature

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,14 +1,30 @@
-export const addItemToCart =(cartItems, cartItemToAdd) => {
-    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id)
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
 
-    if(existingCartItem){
-        return cartItems.map(cartItem => 
-            cartItem.id === cartItemToAdd.id
-            ?
-            {...cartItem,quantity: cartItem.quantity + 1}
-            : cartItem
-        )
-    }
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
 
-    return [...cartItems, {...cartItemToAdd, quantity: 1}]
-}
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cardItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    (item) => item.id === cardItemToRemove.id
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((item) => item.id !== cardItemToRemove.id);
+  }
+  return cartItems.map((cartItem) =>
+    cartItem.id === cardItemToRemove.id
+      ? { ...cartItem, quantity: cardItemToRemove.quantity - 1 }
+      : cartItem
+  );
+};
